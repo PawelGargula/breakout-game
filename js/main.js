76010a565a9f9f6f670208ea0767e7da7cc6a0ce
Paweł Gargula <pawel.gargula@interia.pl>
2023-11-28 20:12:09 +0100

    Add first level of game

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
-const game = new Phaser.Game(480, 320, Phaser.CANVAS, null, { preload: preload, create: create, update: update });
+const game = new Phaser.Game(580, 720, Phaser.CANVAS, null, { preload: preload, create: create, update: update });
 window.addEventListener("deviceorientation", handleOrientation, true);
 let ball;
-const ballVelocity = 200;
+const ballVelocity = 350;
 let brickInfo;
 let bricks;
 let isPortrait = window.matchMedia("(orientation: portrait)").matches;
@@ -16,21 +16,80 @@ let scoreText;
 let score = 0;
 let startButton;
 
+const cosmosArr = new Map();
+cosmosArr.set("0,3", "yellowBrick");
+cosmosArr.set("0,4", "grayBrick");
+cosmosArr.set("0,5", "grayBrick");
+cosmosArr.set("0,6", "grayBrick");
+cosmosArr.set("0,7", "grayBrick");
+cosmosArr.set("1,2", "orangeBrick");
+cosmosArr.set("1,3", "yellowBrick");
+cosmosArr.set("1,4", "lightGrayBrick");
+cosmosArr.set("1,5", "lightGrayBrick");
+cosmosArr.set("1,6", "lightGrayBrick");
+cosmosArr.set("1,7", "lightGrayBrick");
+cosmosArr.set("1,8", "grayBrick");
+cosmosArr.set("2,1", "redBrick");
+cosmosArr.set("2,2", "orangeBrick");
+cosmosArr.set("2,3", "yellowBrick");
+cosmosArr.set("2,4", "lightGrayBrick");
+cosmosArr.set("2,5", "lightGrayBrick");
+cosmosArr.set("2,6", "lightGrayBrick");
+cosmosArr.set("2,7", "lightGrayBrick");
+cosmosArr.set("2,8", "lightGrayBrick");
+cosmosArr.set("2,9", "grayBrick");
+cosmosArr.set("3,0", "redBrick");
+cosmosArr.set("3,1", "redBrick");
+cosmosArr.set("3,2", "orangeBrick");
+cosmosArr.set("3,3", "yellowBrick");
+cosmosArr.set("3,4", "lightGrayBrick");
+cosmosArr.set("3,5", "blueBrick");
+cosmosArr.set("3,6", "lightGrayBrick");
+cosmosArr.set("3,7", "lightGrayBrick");
+cosmosArr.set("3,8", "lightGrayBrick");
+cosmosArr.set("3,9", "lightGrayBrick");
+cosmosArr.set("3,10", "grayBrick");
+cosmosArr.set("4,1", "redBrick");
+cosmosArr.set("4,2", "orangeBrick");
+cosmosArr.set("4,3", "yellowBrick");
+cosmosArr.set("4,4", "lightGrayBrick");
+cosmosArr.set("4,5", "lightGrayBrick");
+cosmosArr.set("4,6", "lightGrayBrick");
+cosmosArr.set("4,7", "lightGrayBrick");
+cosmosArr.set("4,8", "lightGrayBrick");
+cosmosArr.set("4,9", "grayBrick");
+cosmosArr.set("5,2", "orangeBrick");
+cosmosArr.set("5,3", "yellowBrick");
+cosmosArr.set("5,4", "lightGrayBrick");
+cosmosArr.set("5,5", "lightGrayBrick");
+cosmosArr.set("5,6", "lightGrayBrick");
+cosmosArr.set("5,7", "lightGrayBrick");
+cosmosArr.set("5,8", "grayBrick");
+cosmosArr.set("6,3", "yellowBrick");
+cosmosArr.set("6,4", "grayBrick");
+cosmosArr.set("6,5", "grayBrick");
+cosmosArr.set("6,6", "grayBrick");
+cosmosArr.set("6,7", "grayBrick");
+
 function preload() {
     game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
     game.scale.pageAlignHorizontally = true;
     game.scale.pageAlignVertically = true;
-    game.stage.backgroundColor = '#eee';
-    game.load.image('paddle', 'img/paddle.png');
-    game.load.image('brick', 'img/brick.png');
-    game.load.spritesheet('ball', 'img/wobble.png', 20, 20);
+    game.stage.backgroundColor = '#000';
+    game.load.image('paddle', 'img/kosmos/kosmos_paddle.bmp', 104, 24);
+    game.load.image('redBrick', 'img/brick_red.bmp');
+    game.load.image('orangeBrick', 'img/brick_orange.bmp');
+    game.load.image('yellowBrick', 'img/brick_yellow.bmp');
+    game.load.image('grayBrick', 'img/brick_gray.bmp');
+    game.load.image('lightGrayBrick', 'img/brick_light_gray.bmp');
+    game.load.image('blueBrick', 'img/brick_blue.bmp');
+    game.load.spritesheet('ball', 'img/kosmos/ball.bmp', 16, 16);
     game.load.spritesheet('button', 'img/button.png', 120, 40);
 }
 function create() {
     game.physics.startSystem(Phaser.Physics.ARCADE);
     game.physics.arcade.checkCollision.down = false;
-    ball = game.add.sprite(game.world.width * 0.5, game.world.height - 25, 'ball');
-    ball.animations.add('wobble', [0, 1, 0, 2, 0, 1, 0, 2, 0], 24);
+    ball = game.add.sprite(game.world.width * 0.5, game.world.height - 52, 'ball');
     ball.anchor.set(0.5);
     game.physics.enable(ball, Phaser.Physics.ARCADE);
     ball.body.collideWorldBounds = true;
@@ -38,14 +97,14 @@ function create() {
     ball.checkWorldBounds = true;
     ball.events.onOutOfBounds.add(ballLeaveScreen, this);
 
-    paddle = game.add.sprite(game.world.width * 0.5, game.world.height - 5, 'paddle');
+    paddle = game.add.sprite(game.world.width * 0.5, game.world.height - 20, 'paddle');
     paddle.anchor.set(0.5, 1);
     game.physics.enable(paddle, Phaser.Physics.ARCADE);
     paddle.body.immovable = true;
 
     initBricks();
 
-    textStyle = { font: '18px Arial', fill: '#0095DD' };
+    textStyle = { font: '18px Arial', fill: '#FFF' };
     scoreText = game.add.text(5, 5, 'Points: 0', textStyle);
     livesText = game.add.text(game.world.width - 5, 5, 'Lives: ' + lives, textStyle);
     livesText.anchor.set(1, 0);
@@ -67,48 +126,46 @@ function update() {
     if (!playing) {
         return;
     } else if (keys.left.isDown && paddle.x > paddle.width / 2) {
-        paddle.x -= 2;
+        paddle.x -= 4;
     } else if (keys.right.isDown && paddle.x < game.width - paddle.width / 2) {
-        paddle.x += 2;
+        paddle.x += 4;
     }
 }
 function initBricks() {
     brickInfo = {
-        width: 50,
+        width: 36,
         height: 20,
         count: {
-            row: 7,
-            col: 3
+            col: 11,
+            row: 9
         },
         offset: {
-            top: 50,
+            top: 60,
             left: 60
         },
         padding: 10
     }
     bricks = game.add.group();
-    for (c = 0; c < brickInfo.count.col; c++) {
-        for (r = 0; r < brickInfo.count.row; r++) {
-            const brickX = (r * (brickInfo.width + brickInfo.padding)) + brickInfo.offset.left;
-            const brickY = (c * (brickInfo.height + brickInfo.padding)) + brickInfo.offset.top;
-            newBrick = game.add.sprite(brickX, brickY, 'brick');
-            game.physics.enable(newBrick, Phaser.Physics.ARCADE);
-            newBrick.body.immovable = true;
-            newBrick.anchor.set(0.5);
-            bricks.add(newBrick);
+    for (c = 0; c < brickInfo.count.row; c++) {
+        for (r = 0; r < brickInfo.count.col; r++) {
+            const coordinates = `${c},${r}`;
+            if (cosmosArr.has(coordinates)) {
+                const brickX = (r * (brickInfo.width + brickInfo.padding)) + brickInfo.offset.left;
+                const brickY = (c * (brickInfo.height + brickInfo.padding)) + brickInfo.offset.top;
+                newBrick = game.add.sprite(brickX, brickY, cosmosArr.get(coordinates));
+                game.physics.enable(newBrick, Phaser.Physics.ARCADE);
+                newBrick.body.immovable = true;
+                newBrick.anchor.set(0.5);
+                bricks.add(newBrick);
+            }
         }
     }
 }
 function ballHitBrick(ball, brick) {
-    const killTween = game.add.tween(brick.scale);
-    killTween.to({ x: 0, y: 0 }, 200, Phaser.Easing.Linear.None);
-    killTween.onComplete.addOnce(function () {
-        brick.kill();
-    }, this);
-    killTween.start();
+    brick.kill();
     score += 10;
     scoreText.setText('Points: ' + score);
-    if (score === brickInfo.count.row * brickInfo.count.col * 10) {
+    if (score === cosmosArr.size * 10) {
         alert('You won the game, congratulations!');
         location.reload();
     }
@@ -119,8 +176,8 @@ function ballLeaveScreen() {
     if (lives) {
         livesText.setText('Lives: ' + lives);
         lifeLostText.visible = true;
-        ball.reset(game.world.width * 0.5, game.world.height - 25);
-        paddle.reset(game.world.width * 0.5, game.world.height - 5);
+        ball.reset(game.world.width * 0.5, game.world.height - 40);
+        paddle.reset(game.world.width * 0.5, game.world.height - 20);
         game.input.onDown.addOnce(function () {
             lifeLostText.visible = false;
             playing = true;
@@ -133,7 +190,6 @@ function ballLeaveScreen() {
     }
 }
 function ballHitPaddle(ball, paddle) {
-    ball.animations.play('wobble');
     ball.body.velocity.x = -1 * 5 * (paddle.x - ball.x);
 }
 function startGame() {
